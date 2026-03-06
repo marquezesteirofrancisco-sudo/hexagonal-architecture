05e094475ffc315cccf9530353673273d51a14ce
Your Name <you@example.com>
2026-03-06 11:54:46 +0100

    Creación de Caso de Uso NoteMapperService y uso de abstracción IMapper

---
+++ ApplicationComponent/NoteMapperService.cs
@@ -0,0 +1,36 @@
+﻿using Domain_Component.Entities;
+using DomainComponent.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationComponent
+{
+    public class NoteMapperService<TDTO, TModel> : IAddService<TDTO, TModel>
+    {
+        private readonly IAddRepository<TModel> _repository;
+        private readonly IMapper<TDTO, Note> _mapperEntity;
+        private readonly IMapper<TDTO, TModel> _mapperModel;
+
+
+        public NoteMapperService(IAddRepository<TModel> repository, IMapper<TDTO, Note> mapperEntity, IMapper<TDTO, TModel> mapperModel)
+        {
+            _repository = repository;
+            _mapperEntity = mapperEntity;
+            _mapperModel = mapperModel;
+        }
+
+        public async Task AddAsync(TDTO dto)
+        {
+            var note = _mapperEntity.Map(dto);
+            var noteModel = _mapperModel.Map(dto);
+
+            if (note.Message.Length > 3)
+                throw new Exception("El mensaje de tener mas de 3 letras.");
+                
+            await _repository.AddAsync(noteModel);
+        }
+    }
+}
