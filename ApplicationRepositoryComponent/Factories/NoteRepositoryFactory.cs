734733d2fa7d75a6e74c3e85778cf51491485825
Your Name <you@example.com>
2026-03-06 13:12:15 +0100

    Creación de Fabrica de Repositorios

---
+++ ApplicationRepositoryComponent/Factories/NoteRepositoryFactory.cs
@@ -0,0 +1,25 @@
+﻿using Domain_Component.Entities;
+using DomainComponent.Interfaces;
+using InfrastructureRepositoryComponent.ExtraData;
+using RepositoryComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureRepositoryComponent.Factories
+{
+    public class NoteRepositoryFactory : IRepositoryFactory<IAddRepository<Note>, NoteExtraData>
+    {
+        private readonly ItemsDBContext _context;
+
+        public NoteRepositoryFactory(ItemsDBContext context)
+        {
+            _context = context;
+        }
+
+        public IAddRepository<Note> Create(NoteExtraData extraData)
+            => new NoteFactoriedRepository(extraData, _context);
+    }
+}   
