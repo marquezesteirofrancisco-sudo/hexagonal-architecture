7a545363dea49a8b66086c3265c3f96a15ba2414
Your Name <you@example.com>
2026-03-06 12:12:36 +0100

    Creación de Mapper que convierte a Model

---
+++ MapperComponent/NoteModelMapper.cs
@@ -0,0 +1,25 @@
+﻿using Application_RepositoryComponent.Models;
+using ApplicationComponent;
+using ApplicationComponent.DTOs;
+using Domain_Component.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperComponent
+{
+    public class NoteModelMapper : IMapper<NoteDTO, NoteModel>
+    {
+        public NoteModel Map(NoteDTO dto)
+            => new NoteModel()
+            {
+                Id= dto.Id,
+                ItemId = dto.ItemId,
+                Message = dto.Message,
+                CreateDate = DateTime.Now,
+                Color = dto.Color
+            };
+    }
+}
