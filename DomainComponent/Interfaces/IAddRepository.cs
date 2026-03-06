c1097360fad4f7c3a39533f195b6292a75a137bf
Your Name <you@example.com>
2026-03-06 11:42:15 +0100

    Creación de Puerto Secundario IAddRepository

---
+++ DomainComponent/Interfaces/IAddRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainComponent.Interfaces
+{
+    public interface IAddRepository<TModel>
+    {
+        Task AddAsync(TModel model);
+    }
+}
