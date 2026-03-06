2688f672cdaf36bd1556f3daf056c059e2d2ea2b
Your Name <you@example.com>
2026-03-06 12:08:10 +0100

    Creación de Mapper que convierte a Entity

---
+++ MapperComponent/NoteEntityMapper.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationComponent;
+using ApplicationComponent.DTOs;
+using Domain_Component.Entities;
+
+namespace MapperComponent
+{
+    public class NoteEntityMapper : IMapper<NoteDTO, Note>
+    {
+        public Note Map(NoteDTO dto)
+            => new Note(dto.Id, dto.ItemId, dto.Message);
+    }
+}
+    
