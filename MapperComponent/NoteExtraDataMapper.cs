c9a6196e916ead31cecc8a5542b537cc92a13893
Your Name <you@example.com>
2026-03-06 16:58:09 +0100

    Mapper que separa la Información Extra

---
+++ MapperComponent/NoteExtraDataMapper.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationComponent;
+using ApplicationComponent.DTOs;
+using InfrastructureRepositoryComponent.ExtraData;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperComponent
+{
+    public class NoteExtraDataMapper : IMapper<NoteDTO, NoteExtraData>
+    {
+        public NoteExtraData Map(NoteDTO dto)
+            => new NoteExtraData()
+            {
+                Color = dto.Color,
+            };
+    }
+}
