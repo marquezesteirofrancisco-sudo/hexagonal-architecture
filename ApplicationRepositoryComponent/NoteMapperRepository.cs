be335a72b672c634502de96bae93596973bc2225
Your Name <you@example.com>
2026-03-06 12:03:42 +0100

    Creación de Adaptador Secundario NoteMapperRepository

---
+++ ApplicationRepositoryComponent/NoteMapperRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application_RepositoryComponent.Models;
+using DomainComponent.Interfaces;
+using RepositoryComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationRepositoryComponent
+{
+    public class NoteMapperRepository: IAddRepository<NoteModel>
+    {
+        private readonly ItemsDBContext _context;
+
+        public NoteMapperRepository(ItemsDBContext context)
+            => _context = context;
+
+        public async Task AddAsync(NoteModel model)
+        {
+            _context.NotesModel.Add(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
