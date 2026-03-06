dd17efccc618e7c0f458aef0b3dc3d67be8e0cd8
Your Name <you@example.com>
2026-03-06 12:40:18 +0100

    Creación de Puerto Secundario para Fábricas

---
+++ DomainComponent/Interfaces/IRepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainComponent.Interfaces
+{
+    public interface IRepositoryFactory<TRepository, TExtraData>
+    {
+        public TRepository Create(TExtraData extraData);
+
+    }
+}
